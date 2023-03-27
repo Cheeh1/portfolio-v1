1f6383a95db40eab905e67523f7948a73b824536
Divine Edwin <edwindivine5@gmail.com>
2023-03-27 22:09:32 +0100

    fix the className wrong syntax

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -15,35 +15,35 @@ const Content = () => {
 
                 <div className='mt-32 flex justify-evenly'>
                     <div
-                        class="w-96 h-full border-solid border-1 rounded bg-white overflow-hidden shadow-md relative cursor-pointer">
-                        <img class="w-96 h-80 sm:48 object-cover" src={item1} alt="item" />
-                        <div class="mt-4 ml-10 flex flex-col gap-3">
-                            <span class="text-xl font-semibold">Agency Website.</span>
+                        className="w-96 h-full border-solid border-1 rounded bg-white overflow-hidden shadow-md">
+                        <img className="w-96 h-80 sm:48 object-cover" src={item1} alt="item" />
+                        <div className="mt-4 ml-10 mb-5 flex flex-col gap-3">
+                            <span className="text-xl text-cinder-dark font-semibold">Agency Website.</span>
                             <div className='flex gap-5 items-center'>
-                                <span class="text-sm border rounded-md p-2">Wordpress</span>
-                                <img src={link} alt="link-icon" />
+                                <span className="text-sm border text-cinder-light rounded-md p-2">Wordpress</span>
+                                <a href='#link1' target="_blank"><img src={link} alt="link-icon" /></a>
                             </div>
                         </div>
                     </div>
                     <div
-                        class="w-96 h-full border-solid border-1 rounded bg-white overflow-hidden shadow-md relative cursor-pointer">
-                        <img class="w-96 h-80 sm:48 object-cover" src={item2} alt="item" />
-                        <div class="mt-4 ml-10 flex flex-col gap-3">
-                            <span class="text-xl font-semibold">Dashboard Website.</span>
+                        className="w-96 h-full border-solid border-1 rounded bg-white overflow-hidden shadow-md">
+                        <img className="w-96 h-80 sm:48 object-cover" src={item2} alt="item" />
+                        <div className="mt-4 ml-10 mb-5 flex flex-col gap-3">
+                            <span className="text-xl text-cinder-dark font-semibold">Dashboard Website.</span>
                             <div className='flex gap-5 items-center'>
-                                <span class="text-sm border rounded-md p-2">React</span>
-                                <img src={link} alt="link-icon" />
+                                <span className="text-sm text-cinder-light border rounded-md p-2">React</span>
+                                <a href='#link2' target="_blank"><img src={link} alt="link-icon" /></a>
                             </div>
                         </div>
                     </div>
                     <div
-                        class="w-96 h-full border-solid border-1 rounded bg-white overflow-hidden shadow-md relative cursor-pointer">
-                        <img class="w-96 h-80 sm:48 object-cover" src={item3} alt="item" />
-                        <div class="mt-4 ml-10 flex flex-col gap-3">
-                            <span class="text-xl font-semibold">Support Center Website.</span>
+                        className="w-96 h-full border-solid border-1 rounded bg-white overflow-hidden shadow-md">
+                        <img className="w-96 h-80 sm:48 object-cover" src={item3} alt="item" />
+                        <div className="mt-4 ml-10 mb-5 flex flex-col gap-3">
+                            <span className="text-xl text-cinder-dark font-semibold">Support Center Website.</span>
                             <div className='flex gap-5 items-center'>
-                                <span class="text-sm border rounded-md p-2">PHP</span>
-                                <img src={link} alt="link-icon" />
+                                <span className="text-sm text-cinder-light border rounded-md p-2">PHP</span>
+                                <a href='#link3' target="_blank"><img src={link} alt="link-icon" /></a>
                             </div>
                         </div>
                     </div>
