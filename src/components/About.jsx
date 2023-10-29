87564482e677dd1bfbfdb24c615d2db9e8006dbd
Divine Edwin <edwindivine5@gmail.com>
2023-10-29 20:25:59 +0100

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,59 +1,60 @@
 import React from "react";
+import { Skills } from "../data/data";
 
-const Skills = [
-  {
-    icon: "fa-html5",
-    name: "HTML"
-  },
-  {
-    icon: "fa-css3",
-    name: "CSS"
-  },
-  {
-    icon: "fa-js",
-    name: "JAVASCRIPT"
-  },
-  {
-    icon: "fa-react",
-    name: "REACT"
-  },
-  {
-    icon: "fa-git-alt",
-    name: "GIT"
-  },
-  {
-    icon: "fa-sass",
-    name: "SASS"
-  },
-  {
-    icon: "../src/assets/icons/redux.svg",
-    name: "RTK"
-  },
-  {
-    icon: "../src/assets/icons/ts-logo.svg",
-    name: "TYPESCRIPT"
-  },
-  {
-    icon: "../src/assets/icons/Vitejs.svg",
-    name: "VITE"
-  },
-  {
-    icon: "../src/assets/icons/npm.svg",
-    name: "NPM"
-  },
-  {
-    icon: "../src/assets/icons/firebase.svg",
-    name: "FIREBASE"
-  },
-  {
-    icon: "../src/assets/icons/tailwindcss.svg",
-    name: "TAILWIND"
-  },
-  {
-    icon: "fa-github",
-    name: "GITHUB"
-  },
-]
+// const Skills = [
+//   {
+//     icon: "fa-html5",
+//     name: "HTML"
+//   },
+//   {
+//     icon: "fa-css3",
+//     name: "CSS"
+//   },
+//   {
+//     icon: "fa-js",
+//     name: "JAVASCRIPT"
+//   },
+//   {
+//     icon: "fa-react",
+//     name: "REACT"
+//   },
+//   {
+//     icon: "fa-git-alt",
+//     name: "GIT"
+//   },
+//   {
+//     icon: "fa-sass",
+//     name: "SASS"
+//   },
+//   {
+//     icon: "../src/assets/icons/redux.svg",
+//     name: "RTK"
+//   },
+//   {
+//     icon: "../src/assets/icons/ts-logo.svg",
+//     name: "TYPESCRIPT"
+//   },
+//   {
+//     icon: "../src/assets/icons/Vitejs.svg",
+//     name: "VITE"
+//   },
+//   {
+//     icon: "../src/assets/icons/npm.svg",
+//     name: "NPM"
+//   },
+//   {
+//     icon: "../src/assets/icons/firebase.svg",
+//     name: "FIREBASE"
+//   },
+//   {
+//     icon: "../src/assets/icons/tailwindcss.svg",
+//     name: "TAILWIND"
+//   },
+//   {
+//     icon: "fa-github",
+//     name: "GITHUB"
+//   },
+// ]
 
 const About = () => {
   return (
