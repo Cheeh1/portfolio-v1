77dcf234c012d9f4980823d68a8f7577a47d31a2
divine edwin <edwindivine5@gmail.com>
2025-11-12 16:57:39 +0100

    feat: added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from "./App";
 import "./index.css";
 import 'aos/dist/aos.css';
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Analytics />
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
